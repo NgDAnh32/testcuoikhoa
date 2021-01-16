dfc0dd0fdeb23c810635dd1be9c2542631f27d6c
NgDAnh32 <74419094+NgDAnh32@users.noreply.github.com>
2021-01-16 18:39:09 +0700

    update test

--- mindtest.js
+++ mindtest.js
@@ -1,4 +1,12 @@
+// bài 1:
+function findOppositeNumber(n,inputNumber) {
+    if (inputNumber<n/2)
+    return inputNumber+(n/2)
 
+    else return inputNumber-(n/2)
+}
+console.log(findOppositeNumber(10,6))
+console.log(findOppositeNumber(10,2))
 // bài 2
 function merge2String(a,b) {
     let result="";
@@ -17,4 +25,17 @@ function merge2String(a,b) {
 console.log(merge2String("abc","123"));
 console.log(merge2String("abc","0123"));
 console.log(merge2String("abcd","123"));
-
+// bài 3 :
+function lucky(){
+    let randomNumber= Math.floor(Math.random() * 11);
+    console.log(randomNumber);
+    let button=document.getElementById("submitBtn").addEventListener("click",()=>{
+        let input=document.getElementById("input").value
+        if(input==""){
+            alert("xin mời nhập lại ")
+        }if(input>10){
+            alert("xin mời nhập lại số từ 0 đến 10")
+        }
+    })
+}
+lucky()
